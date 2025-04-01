2ee9e4407fc8fdda26d84aeb4169e97d4d0cf00b
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-01 16:11:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species;
+	}
+	get species(){
+		return this.species;
+	}
+	makeSound(){
+		console.log(`The ${this.species} makes a sound`);
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
+	bark(){
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr(){
+		console.log("purr");
+	}
+}
+const myCat=new Cat("Siamese");
+myCat.makeSound();
+myCat.purr();
+
+const myDog=new Dog("Golden Retriever");
+myDog.makeSound();
+myDog.bark();
+
 
 // Do not change the code below this line
 window.Animal = Animal;
